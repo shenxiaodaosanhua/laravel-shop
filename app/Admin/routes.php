cd1f86ef32346bfc4e68e769247d7dcc8cfc01ec
shenxiaosanhua <87976956@qq.com>
2018-07-05 00:42:48 +0800

    管理后台 - 新增、编辑商品

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,6 +13,6 @@ Route::group([
     $router->get('/', 'HomeController@index');
     $router->get('users', 'UsersController@index');
 
-    $router->get('products', 'ProductsController@index');
+    $router->resource('products', 'ProductsController');
 
 });
