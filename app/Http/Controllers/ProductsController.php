233ec193ecbf7d391fe21a22d7c4e36a6b42afd4
shenxiaosanhua <87976956@qq.com>
2018-07-05 01:39:14 +0800

    用户界面 - 收藏商品列表

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -92,5 +92,12 @@ class ProductsController extends Controller
         return [];
     }
 
+    public function favorites(Request $request)
+    {
+        $products = $request->user()->favoriteProducts()->paginate(16);
+
+        return view('products.favorites', ['products' => $products]);
+    }
+
 
 }
