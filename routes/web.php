9d616fd9e4924cb631157e765759a72b74bced6d
shenxiaosanhua <87976956@qq.com>
2018-07-10 15:25:49 +0800

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,8 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('cart', 'CartController@index')->name('cart.index');
         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 
+        Route::post('orders', 'OrdersController@store')->name('orders.store');
+
     });
 
 });
