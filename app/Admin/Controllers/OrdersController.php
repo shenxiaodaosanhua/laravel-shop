5106e72027fb3af66cbe65adbb9776e290386ba4
shenxiaosanhua <87976956@qq.com>
2018-07-12 17:25:17 +0800

    管理后台 - 拒绝退款

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Exceptions\InvalidRequestException;
+use App\Http\Requests\Admin\HandleRefundRequest;
 use Illuminate\Http\Request;
 use App\Models\Order;
 
@@ -32,6 +33,10 @@ class OrdersController extends Controller
         });
     }
 
+    /**
+     * @param Order $order
+     * @return Content
+     */
     public function show(Order $order)
     {
         return Admin::content(function (Content $content) use ($order) {
@@ -75,6 +80,36 @@ class OrdersController extends Controller
 
         // 返回上一页
         return redirect()->back();
+    }
+
+    /**
+     * @param Order $order
+     * @param HandleRefundRequest $request
+     * @return Order
+     * @throws InvalidRequestException
+     */
+    public function handleRefund(Order $order, HandleRefundRequest $request)
+    {
+        // 判断订单状态是否正确
+        if ($order->refund_status !== Order::REFUND_STATUS_APPLIED) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+        // 是否同意退款
+        if ($request->input('agree')) {
+            // 同意退款的逻辑这里先留空
+            // todo
+        } else {
+            // 将拒绝退款理由放到订单的 extra 字段中
+            $extra = $order->extra ?: [];
+            $extra['refund_disagree_reason'] = $request->input('reason');
+            // 将订单的退款状态改为未退款
+            $order->update([
+                'refund_status' => Order::REFUND_STATUS_PENDING,
+                'extra'         => $extra,
+            ]);
+        }
+
+        return $order;
     }
 
     /**
