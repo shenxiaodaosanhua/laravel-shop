6879533ab2507c7170d71107767f6a1038f816d5
shenxiaosanhua <87976956@qq.com>
2018-07-12 16:59:33 +0800

    评价商品

---
+++ app/Events/OrderReviewd.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
+class OrderReviewd
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * @var Order
+     */
+    protected $order;
+
+    /**
+     * OrderReviewd constructor.
+     * @param Order $order
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * @return Order
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+}
